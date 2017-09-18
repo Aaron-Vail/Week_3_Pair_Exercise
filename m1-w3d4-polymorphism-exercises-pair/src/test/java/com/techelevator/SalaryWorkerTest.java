b4178ec9e812459e7ce1cc4e5d7557ddd1927107
Aaron <aaron.vail@yahoo.com>
2017-09-18 08:17:18 -0400

    vail, aaron t1w3 exercise final

--- m1-w3d4-polymorphism-exercises-pair/src/test/java/com/techelevator/SalaryWorkerTest.java
+++ m1-w3d4-polymorphism-exercises-pair/src/test/java/com/techelevator/SalaryWorkerTest.java
@@ -1,10 +1,5 @@
 package com.techelevator;
 
-import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 	
 public class SalaryWorkerTest {
 	SalaryWorker sut;
